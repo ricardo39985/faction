2fcf3eaf0acbbbce4dd4c838330615e835014f94
ricardo1181 <ricardo39985@gmail.com>
2022-10-10 12:28:57 -0400

    Add routes for google oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var router = require('express').Router();
+const passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  console.log(req.user);
+  res.render('index', { title: 'Express', user: req.user });
+});
+// Google OAuth login route
+router.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  })
+);
+
+// Google OAuth callback route
+router.get(
+  '/oauth2callback',
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/',
+  })
+);
+// OAuth logout route
+router.get('/logout', function (req, res) {
+  req.logout(() => {});
+  res.redirect('/');
 });
 
 module.exports = router;
