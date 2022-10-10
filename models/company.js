fa093ae23c342beed8f98694a1ff82f4677343dc
ricardo1181 <ricardo39985@gmail.com>
2022-10-10 12:49:05 -0400

    Add company model

---
+++ models/company.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const companySchema = new Schema({
+  name: String,
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
+  phone: Number
+},{
+    timestamps:true
+});
