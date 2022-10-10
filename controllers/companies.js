26db527c0a406f934765a158a3d262114b4b27ce
ricardo1181 <ricardo39985@gmail.com>
2022-10-10 16:34:23 -0400

    Import model,implement create, create edit fucntion

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,16 +1,23 @@
+const Company = require('../models/company');
 module.exports = {
   new: newCompany,
   create,
   show,
   update,
   delete: deleteCompany,
+  index,
+  edit,
 };
 
 function newCompany(req, res) {
   res.render('companies/new');
 }
 function create(req, res) {
-  res.redirect('/');
+  const newCompany = new Company(req.body);
+  newCompany.user = req.user;
+  newCompany.save(function (err) {
+    res.redirect(`/companies/${newCompany._id}`);
+  });
 }
 function deleteCompany(req, res) {
   res.redirect('/');
@@ -21,3 +28,9 @@ function show(req, res) {
 function update(req, res) {
   res.render('companies/update');
 }
+function index(req, res) {
+  Company.find({ user: req.user }, function (err, companies) {
+    res.render('companies/index', { companies });
+  });
+}
+function edit(req, res) {}
