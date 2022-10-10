2310cfea88a7f24345874fc4650aa0cebffbbec7
ricardo1181 <ricardo39985@gmail.com>
2022-10-10 17:17:58 -0400

    Use companies router

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/', companiesRouter)
+app.use('/companies', companiesRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
