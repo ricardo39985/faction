688d6622340caafd1c33e55cd399d01d8f9c9c4f
ricardo1181 <ricardo39985@gmail.com>
2022-10-10 17:53:08 -0400

    Format Spacing

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log(req.user);
   res.render('index', { title: 'Express', user: req.user });
 });
 // Google OAuth login route
