8aa93b22a18db86145431c8bc27105eea60e5e51
ricardo1181 <ricardo39985@gmail.com>
2022-10-11 13:24:36 -0400

    Update spacing/code format

--- models/company.js
+++ models/company.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const companySchema = new Schema({
-  name: String,
-  user: {type: Schema.Types.ObjectId, ref: 'User'},
-  phone: Number
-},{
-    timestamps:true
-});
-module.exports = mongoose.model('Company', companySchema)
+const companySchema = new Schema(
+  {
+    name: String,
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    phone: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model('Company', companySchema);
