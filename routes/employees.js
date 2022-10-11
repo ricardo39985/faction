ac1e4c5b7065e1185bd2341214c457e085801354
ricardo1181 <ricardo39985@gmail.com>
2022-10-11 13:52:55 -0400

    Add more verbose param names

---
+++ routes/employees.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const employeesCtrl = require('../controllers/employees');
+const isLoggedIn = require('../config/auth')
+
+router.get('/companies/:companyid/employees', isLoggedIn, employeesCtrl.index);
+router.get('/companies/:companyid/employees/new', isLoggedIn, employeesCtrl.new);
+router.post('/companies/:companyid/employees', isLoggedIn, employeesCtrl.create)
+router.get('/companies/:companyid/employees/:employeeid', isLoggedIn,employeesCtrl.show)
+router.get('/companies/:companyid/employees/:employeeid/edit', isLoggedIn,employeesCtrl.edit)
+router.delete('/companies/:companyid/employees/:employeeid', isLoggedIn,employeesCtrl.delete)
+
+module.exports = router;
