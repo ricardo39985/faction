c84a87b23e57577eac8851189ee6e951bcd9ac07
ricardo1181 <ricardo39985@gmail.com>
2022-10-11 15:30:35 -0400

    Add department field. split name field into firstname and lastname

--- models/employee.js
+++ models/employee.js
@@ -3,13 +3,40 @@ const Schema = mongoose.Schema;
 const employeeScheema = new Schema(
   {
     company: { type: Schema.Types.ObjectId, ref: 'Company' },
-    name: String,
+    firstName: String,
+    lastName: String,
+    email: String,
     rate: Number,
-    department: String,
+    department: {
+      type: String,
+      enum: [
+        'Marketing',
+        'Proposals',
+        'Sales',
+        'Project',
+        'Designing',
+        'Production',
+        'Maintenance',
+        'Store',
+        'Procurement',
+        'Quality',
+        'Inspection',
+        'Packaging',
+        'Finance',
+        'Dispatch',
+        'Account',
+        'Research',
+        'Development',
+        'Information Technology',
+        'Human Resource',
+        'Security',
+        'Administration',
+      ],
+    },
     avatar: String,
   },
   {
     timestamps: true,
   }
 );
-module.exports = mongoose.model('Employee', employeeScheema)
+module.exports = mongoose.model('Employee', employeeScheema);
